7a6e348a08afc3f35b06d613d48d4a3f9749e4fc
Jaakko Ritoluoma <ritoluoma@hotmail.com>
2017-04-11 00:40:59 +0300

    Changed config

--- core/config.php
+++ core/config.php
@@ -1,9 +1,9 @@
 <?php
 return [
 	'database' => [
-		'name' => 'todos',
-		'username' => 'homestead',
-		'password' => 'secret',
+		'name' => 'tiko',
+		'username' => 'root',
+		'password' => '',
 		'connection' => 'mysql:host=127.0.0.1', //'pgsql:host=dbstud2.sis.uta.fi',
 		'options' => [
 			PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
