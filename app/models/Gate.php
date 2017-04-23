65d63c9f289dacb9b308c771ea7e9a2a27b1ad50
DESKTOP-B1LDGN2\Aapo <al422806@student.uta.fi>
2017-04-23 13:19:49 +0300

    Lisättiin gate luokka

---
+++ app/models/Gate.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+
+class Gate
+{
+    public static function hasRole($role){
+        if($_SESSION['opettaja'] == $role){
+            return true;
+        }
+        else if($_SESSION['opiskelija'] == $role){
+            return true;
+        }
+        else if($_SESSION['admin'] == $role){
+            return true;
+        }
+        return false;
+    }
+}
