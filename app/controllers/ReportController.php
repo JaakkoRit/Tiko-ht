eca75b90e7b7826dee56b82f42f203cfe8bd7e78
DESKTOP-B1LDGN2\Aapo <al422806@student.uta.fi>
2017-04-30 21:26:05 +0300

    Kontrolleriin lisättiin construct metodi, jossa tapahtuu rooli checki

--- app/controllers/ReportController.php
+++ app/controllers/ReportController.php
@@ -10,24 +10,20 @@ use App\App\Models\Gate;
 
 class ReportController
 {
-    public function showSessionRaport(){
+    public function __construct(){
         if(Gate::hasRole('opiskelija'))
-            return view('index');
-        else {
-            $tasklistArray = TaskList::findAllWhere("ID_KAYTTAJA", $_SESSION['id_kayttaja']);
-            $sessionArray = Session::all();
-            $studentArray = Student::all();
-            $attemptArray = Attempt::all();
-            return view('session-report', compact('sessionArray', 'attemptArray', 'studentArray', 'tasklistArray'));
-        }
+            header('Location:/student-home');
+    }
+    public function showSessionRaport(){
+        $tasklistArray = TaskList::findAllWhere("ID_KAYTTAJA", $_SESSION['id_kayttaja']);
+        $sessionArray = Session::all();
+        $studentArray = Student::all();
+        $attemptArray = Attempt::all();
+        return view('session-report', compact('sessionArray', 'attemptArray', 'studentArray', 'tasklistArray'));
     }
     public function showTaskListSessionReport(){
-        if(Gate::hasRole('opiskelija'))
-            return view('index');
-        else{
-            $tasklistArray = TaskList::findAllWhere("ID_KAYTTAJA", $_SESSION['id_kayttaja']);
-            $sessionArray = Session::all();
-            return view('tasklistsession-report', compact('tasklistArray', 'sessionArray'));
-        }
+        $tasklistArray = TaskList::findAllWhere("ID_KAYTTAJA", $_SESSION['id_kayttaja']);
+        $sessionArray = Session::all();
+        return view('tasklistsession-report', compact('tasklistArray', 'sessionArray'));
     }
 }
