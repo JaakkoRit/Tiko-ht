0b30922bd7a0acb8d40b582b4a55386aced7c135
DESKTOP-B1LDGN2\Aapo <al422806@student.uta.fi>
2017-05-03 18:17:21 +0300

    student controllerin hasrole sääntöjä muutettiin hieman

--- app/controllers/StudentController.php
+++ app/controllers/StudentController.php
@@ -10,38 +10,46 @@ use App\App\Models\Gate;
 
 class StudentController
 {
-    public function __construct(){
-        if(Gate::hasRole('opettaja'))
-            header('Location:/teacher-home');
-    }
     public function index()
     {
-        $sessions = Session::findAllWhere('ID_KAYTTAJA', auth()->ID_KAYTTAJA);
+        if(Gate::hasRole('opettaja'))
+            header('Location:/teacher-home');
+        else{
+            $sessions = Session::findAllWhere('ID_KAYTTAJA', auth()->ID_KAYTTAJA);
 
-        return view('student-home', compact('sessions'));
+            return view('student-home', compact('sessions'));
+        }
     }
 
     public function create()
     {
-        return view('login');
+        if(Gate::hasRole('opettaja'))
+            header('Location:/teacher-home');
+        else
+            return view('login');
     }
 
     public function save()
     {
-        $req = App::get('request');
-        $student = Student::findWhere('ONRO', $req->get('onro'));
-        $user = User::find($student->ID_KAYTTAJA);
+        if(Gate::hasRole('opettaja'))
+            header('Location:/teacher-home');
+        else{
+            $req = App::get('request');
+            $student = Student::findWhere('ONRO', $req->get('onro'));
+            $user = User::find($student->ID_KAYTTAJA);
 
-        if ($student && password_verify($req->get('salasana'), $student->SALASANA)) {
-            $_SESSION['id_kayttaja'] = $student->ID_KAYTTAJA;
-            $_SESSION['nimi'] = $student->NIMI;
-            $_SESSION['onro'] = $student->ONRO;
-            $_SESSION['rooli'] = $user->ROOLI;
+            if ($student && password_verify($req->get('salasana'), $student->SALASANA)) {
+                $_SESSION['id_kayttaja'] = $student->ID_KAYTTAJA;
+                $_SESSION['nimi'] = $student->NIMI;
+                $_SESSION['onro'] = $student->ONRO;
+                $_SESSION['rooli'] = $user->ROOLI;
 
-            header('Location: /student-home');
+                header('Location: /student-home');
+            }
+
+            return view('login', ['message' => 'Opiskelijanumero tai salasana väärin.']);
         }
 
-        return view('login', ['message' => 'Opiskelijanumero tai salasana väärin.']);
     }
 
     public function destroy()
